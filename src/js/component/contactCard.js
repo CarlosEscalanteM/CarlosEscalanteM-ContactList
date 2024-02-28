fe0bb7fb15674f1eefdb04beaaae4c1985f3b971
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-02-28 02:15:07 +0000

    Created-Doc

---
+++ src/js/component/contactCard.js
@@ -0,0 +1,10 @@
+const ContactCard = (props) => {
+return <div>This is my Contact Card
+</div>
+
+
+
+}
+
+export default ContactCard;
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,18 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { useEffect, useState } from "react";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+	const [contactList,setContactList] = useState ([])
+	useEffect (() => {
+fetch ("https://playground.4geeks.com/apis/fake/contact/agenda/CarlosEscalanteM")
+.then (Response => Response.json())
+.then (Result => setContactList(Result))
+	},[])
+	console.log(contactList)
+	return (
+	<div>
+		<button onClick={() => setCounter (Math.random ())}>Add 1</button>
+		</div>
+)};
