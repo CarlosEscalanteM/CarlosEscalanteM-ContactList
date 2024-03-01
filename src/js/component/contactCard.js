ce5312c680ccd84ce423af784179eeb3a3f2a46f
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-03-01 01:39:59 +0000

    Second Attempt

--- src/js/component/contactCard.js
+++ src/js/component/contactCard.js
@@ -1,6 +1,27 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 const ContactCard = (props) => {
-return <div>This is my Contact Card
-</div>
+    return <div className="CardContainer d-flex px-3 border border-secondary justify-content-between align-items-center">
+        <div className="LeftSide d-flex align-items-center py-3">
+            <div className="Image col-3">
+                <img className="rounded-circle img-fluid" src="https://thispersondoesnotexist.com/"></img> </div>
+            <div className="ContactInformation ps-5">
+                <h1>{props.name}</h1>
+                <h1>{props.address}</h1>
+                <h1>{props.phone}</h1>
+                <h1>{props.email}</h1>
+            </div>
+        </div>
+
+        <div className="RightSide">
+            <FontAwesomeIcon icon={faPencil}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+        </div>
+
+
+    </div>
 
 
 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/home.css";
 import { useEffect, useState } from "react";
+import ContactCard from "../component/contactCard";
 
 
 export const Home = () => {
@@ -13,6 +14,6 @@ fetch ("https://playground.4geeks.com/apis/fake/contact/agenda/CarlosEscalanteM"
 	console.log(contactList)
 	return (
 	<div>
-		<button onClick={() => setCounter (Math.random ())}>Add 1</button>
-		</div>
+		{contactList.map (contact => <ContactCard name={contact.full_name} address={contact.address} phone={contact.phone} email={contact.email}></ContactCard>)}
+			</div>
 )};
